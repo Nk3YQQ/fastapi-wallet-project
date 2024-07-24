60d3d39b90992c4c242369450c17c87ca250417b
egor <ega.nik5674@gmail.com>
2024-07-24 22:32:43 +0300

    Init new migrations

---
+++ users/migrations/users/4_20240724130128_None.py
@@ -0,0 +1,45 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "user" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "first_name" VARCHAR(150) NOT NULL,
+    "last_name" VARCHAR(150) NOT NULL,
+    "email" VARCHAR(100) NOT NULL UNIQUE,
+    "password" VARCHAR(100) NOT NULL,
+    "passport_series" INT NOT NULL,
+    "passport_number" INT NOT NULL
+);
+COMMENT ON COLUMN "user"."id" IS 'Первичный ключ';
+COMMENT ON COLUMN "user"."first_name" IS 'Имя';
+COMMENT ON COLUMN "user"."last_name" IS 'Фамилия';
+COMMENT ON COLUMN "user"."email" IS 'Электронная почта';
+COMMENT ON COLUMN "user"."password" IS 'Пароль';
+COMMENT ON COLUMN "user"."passport_series" IS 'Серия паспорта';
+COMMENT ON COLUMN "user"."passport_number" IS 'Номер паспорта';
+COMMENT ON TABLE "user" IS 'Модель для пользователя ';
+CREATE TABLE IF NOT EXISTS "wallet" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "balance" DECIMAL(10,2) NOT NULL,
+    "currency" VARCHAR(3) NOT NULL,
+    "wallet_type" VARCHAR(20) NOT NULL,
+    "owner_id" INT NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE
+);
+COMMENT ON COLUMN "wallet"."id" IS 'Первичный ключ';
+COMMENT ON COLUMN "wallet"."balance" IS 'Баланс';
+COMMENT ON COLUMN "wallet"."currency" IS 'Валюта';
+COMMENT ON COLUMN "wallet"."wallet_type" IS 'Тип кошелька';
+COMMENT ON TABLE "wallet" IS 'Модель для кошелька ';"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """
