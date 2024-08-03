1862db13c60bce890c464789360ecf873a566c45
egor <ega.nik5674@gmail.com>
2024-08-03 13:02:17 +0300

    Init config

---
+++ config/database.py
@@ -0,0 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": {
+            "engine": "tortoise.backends.asyncpg",
+            "credentials": {
+                "database": os.getenv('POSTGRES_DB'),
+                "user": os.getenv('POSTGRES_USER'),
+                "password": os.getenv('POSTGRES_PASSWORD'),
+                "host": os.getenv('POSTGRES_HOST'),
+            }
+        }
+    },
+    "apps": {
+        "models": {
+            "models": ["wallet.models", "users.models"],
+            "default_connection": "default"
+        },
+        "aerich": {
+            "models": ["aerich.models"],
+            "default_connection": "default"
+        }
+    }
+}
